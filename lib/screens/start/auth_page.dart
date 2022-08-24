d3d5e5128b1124e5907aa5c7d279e185e3bae443
kyu <2066apple@naver.com>
2022-08-24 21:29:42 +0900

    ignorePointer, verify에 따른 버튼 상태 변경

--- lib/screens/start/auth_page.dart
+++ lib/screens/start/auth_page.dart
@@ -38,6 +38,18 @@ class _AuthPageState extends State<AuthPage> {
     }
   }
 
+  void changeVerify() async {
+    setState(() {
+      _verifyStatus = VerifyStatus.verifying;
+    });
+
+    await Future.delayed(Duration(seconds: 2));
+
+    setState(() {
+      _verifyStatus = VerifyStatus.done;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
@@ -52,114 +64,115 @@ class _AuthPageState extends State<AuthPage> {
               style: Theme.of(context).appBarTheme.titleTextStyle,
             ),
           ),
-          body: Form(
-            key: _formkey,
-            child: Padding(
-              padding: const EdgeInsets.all(common_padding),
-              child: Column(
-                children: [
-                  Row(
-                    children: [
-                      // image,
-                      ExtendedImage.asset(
-                        'assets/images/padlock.png',
-                        width: size.width * 0.16,
-                        height: size.width * 0.16,
-                      ),
-                      const SizedBox(
-                        width: 8.0,
-                      ),
-                      // text
-                      Text(
-                          '고기 마켓은 휴대폰 번호로 가입해요\n 번호는 안전하게 보관되며\n 어디에도 공개되지 않아요.')
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 16.0,
-                  ),
-                  TextFormField(
-                    controller: _phoneNumberController,
-                    keyboardType: TextInputType.phone,
-                    inputFormatters: [
-                      MaskedInputFormatter('000 0000 0000'),
-                    ],
-                    validator: (phoneNumber) {
-                      if (phoneNumber != null && phoneNumber.length == 13) {
-                        return null;
-                      } else {
-                        // error
-                        return '전화번호 똑바로 입력해주세요.';
-                      }
-                    },
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
+          body: IgnorePointer(
+            ignoring: _verifyStatus == VerifyStatus.verifying,
+            child: Form(
+              key: _formkey,
+              child: Padding(
+                padding: const EdgeInsets.all(common_padding),
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        // image,
+                        ExtendedImage.asset(
+                          'assets/images/padlock.png',
+                          width: size.width * 0.16,
+                          height: size.width * 0.16,
+                        ),
+                        const SizedBox(
+                          width: 8.0,
+                        ),
+                        // text
+                        Text(
+                            '고기 마켓은 휴대폰 번호로 가입해요\n 번호는 안전하게 보관되며\n 어디에도 공개되지 않아요.')
+                      ],
                     ),
-                  ),
-                  const SizedBox(
-                    height: 8.0,
-                  ),
-                  ElevatedButton(
-                      onPressed: () {
-                        // 이거 이렇게 연결 안하면 input에서 validator가 안먹네???? 왤까????
-                        if (_formkey.currentState != null) {
-                          bool passed = _formkey.currentState!.validate();
-                          if (passed) {
-                            setState(() {
-                              _verifyStatus = VerifyStatus.codeSent;
-                            });
-                            logger.d(_verifyStatus);
-                          }
+                    const SizedBox(
+                      height: 16.0,
+                    ),
+                    TextFormField(
+                      controller: _phoneNumberController,
+                      keyboardType: TextInputType.phone,
+                      inputFormatters: [
+                        MaskedInputFormatter('000 0000 0000'),
+                      ],
+                      validator: (phoneNumber) {
+                        if (phoneNumber != null && phoneNumber.length == 13) {
+                          return null;
+                        } else {
+                          // error
+                          return '전화번호 똑바로 입력해주세요.';
                         }
                       },
-                      child: Text('인증문자 받기'),
-                      style: ElevatedButton.styleFrom(
-                        minimumSize: Size.fromHeight(50),
-                      )),
-                  const SizedBox(
-                    height: 16.0,
-                  ),
-                  AnimatedOpacity(
-                    duration: Duration(milliseconds: 300),
-                    opacity: _verifyStatus == VerifyStatus.none ? 0 : 1,
-                    child: AnimatedContainer(
-                      duration: Duration(milliseconds: 500),
-                      curve: Curves.easeInOut,
-                      height: getContainerHeight(_verifyStatus, 'input'),
-                      child: TextFormField(
-                        controller: _codeController,
-                        keyboardType: TextInputType.phone,
-                        inputFormatters: [
-                          MaskedInputFormatter('000000'),
-                        ],
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(),
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 8.0,
+                    ),
+                    ElevatedButton(
+                        onPressed: () {
+                          // 이거 이렇게 연결 안하면 input에서 validator가 안먹네???? 왤까????
+                          if (_formkey.currentState != null) {
+                            bool passed = _formkey.currentState!.validate();
+                            if (passed) {
+                              setState(() {
+                                _verifyStatus = VerifyStatus.codeSent;
+                              });
+                              logger.d(_verifyStatus);
+                            }
+                          }
+                        },
+                        child: Text('인증문자 받기'),
+                        style: ElevatedButton.styleFrom(
+                          minimumSize: Size.fromHeight(50),
+                        )),
+                    const SizedBox(
+                      height: 16.0,
+                    ),
+                    AnimatedOpacity(
+                      duration: Duration(milliseconds: 300),
+                      opacity: _verifyStatus == VerifyStatus.none ? 0 : 1,
+                      child: AnimatedContainer(
+                        duration: Duration(milliseconds: 500),
+                        curve: Curves.easeInOut,
+                        height: getContainerHeight(_verifyStatus, 'input'),
+                        child: TextFormField(
+                          controller: _codeController,
+                          keyboardType: TextInputType.phone,
+                          inputFormatters: [
+                            MaskedInputFormatter('000000'),
+                          ],
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  // const SizedBox(
-                  //   height: 8.0,
-                  // ),
-                  AnimatedOpacity(
-                    duration: Duration(milliseconds: 300),
-                    opacity: _verifyStatus == VerifyStatus.none ? 0 : 1,
-                    child: AnimatedContainer(
-                      duration: Duration(milliseconds: 500),
-                      curve: Curves.easeInOut,
-                      height: getContainerHeight(_verifyStatus, 'button'),
-                      child: ElevatedButton(
-                          onPressed: () {
-                            setState(() {
-                              _verifyStatus = VerifyStatus.none;
-                            });
-                          },
-                          child: Text('인증번호 확인'),
-                          style: ElevatedButton.styleFrom(
-                            minimumSize: Size.fromHeight(50),
-                          )),
+                    // const SizedBox(
+                    //   height: 8.0,
+                    // ),
+                    AnimatedOpacity(
+                      duration: Duration(milliseconds: 300),
+                      opacity: _verifyStatus == VerifyStatus.none ? 0 : 1,
+                      child: AnimatedContainer(
+                        duration: Duration(milliseconds: 500),
+                        curve: Curves.easeInOut,
+                        height: getContainerHeight(_verifyStatus, 'button'),
+                        child: ElevatedButton(
+                            onPressed: () {
+                              changeVerify();
+                            },
+                            child: (_verifyStatus == VerifyStatus.verifying) ? CircularProgressIndicator(color: Colors.white,) : Text('인증번호 확인'),
+                            style: ElevatedButton.styleFrom(
+                              minimumSize: Size.fromHeight(50),
+                            )),
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           ),
