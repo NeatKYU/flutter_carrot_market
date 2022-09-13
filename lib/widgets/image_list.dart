835d7e4ecff0430edc63192468d2c1beaaf37669
kyu <2066apple@naver.com>
2022-09-13 21:33:59 +0900

    ✨ 사진 선택 및 보여주기 기능 추가

--- lib/widgets/image_list.dart
+++ lib/widgets/image_list.dart
@@ -1,9 +1,18 @@
 import 'package:carrot_market_by_flutter/constants/common_size.dart';
 import 'package:extended_image/extended_image.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
-class ImageList extends StatelessWidget {
+class ImageList extends StatefulWidget {
   const ImageList({super.key});
+
+  @override
+  State<ImageList> createState() => _ImageListState();
+}
+
+class _ImageListState extends State<ImageList> {
+  List<XFile> _images = [];
 
   @override
   Widget build(BuildContext context) {
@@ -15,30 +24,43 @@ class ImageList extends StatelessWidget {
       child: ListView(
         scrollDirection: Axis.horizontal,
         children: [
-          Padding(
-            padding: const EdgeInsets.all(common_padding),
-            child: Container(
-                width: imageWidth,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(5),
-                  border: Border.all(color: Colors.grey, width: 1),
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(
-                      Icons.camera_alt_rounded,
-                      color: Colors.grey,
-                    ),
-                    Text(
-                      '0/10',
-                      style: TextStyle(color: Colors.grey),
-                    )
-                  ],
-                )),
+          InkWell(
+            // 사진 추가하기 버튼
+            onTap: () async {
+              final ImagePicker _picker = ImagePicker();
+              final List<XFile>? images = await _picker.pickMultiImage();
+              if (images != null && images.isNotEmpty) {
+                _images.clear();
+                _images.addAll(images);
+                // setState를 해주지않으면 리스트가 반영이 안됨...
+                setState(() {});
+              }
+            },
+            child: Padding(
+              padding: const EdgeInsets.all(common_padding),
+              child: Container(
+                  width: imageWidth,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(5),
+                    border: Border.all(color: Colors.grey, width: 1),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.camera_alt_rounded,
+                        color: Colors.grey,
+                      ),
+                      Text(
+                        '0/10',
+                        style: TextStyle(color: Colors.grey),
+                      )
+                    ],
+                  )),
+            ),
           ),
           ...List.generate(
-            10,
+            _images.length,
             (index) => Stack(
               children: [
                 Padding(
@@ -47,13 +69,26 @@ class ImageList extends StatelessWidget {
                     top: common_padding,
                     bottom: common_padding,
                   ),
-                  child: ExtendedImage.network(
-                    'https://picsum.photos/100',
-                    width: imageWidth,
-                    height: imageWidth,
-                    borderRadius: BorderRadius.circular(common_padding_sm),
-                    shape: BoxShape.rectangle,
-                  ),
+                  child: FutureBuilder<Uint8List>(
+                      future: _images[index].readAsBytes(),
+                      builder: (context, snapshot) {
+                        if (snapshot.hasData) {
+                          return ExtendedImage.memory(
+                            snapshot.data!,
+                            width: imageWidth,
+                            height: imageWidth,
+                            borderRadius:
+                                BorderRadius.circular(common_padding_sm),
+                            shape: BoxShape.rectangle,
+                          );
+                        } else {
+                          return Container(
+                            width: imageWidth,
+                            height: imageWidth,
+                            child: CircularProgressIndicator(),
+                          );
+                        }
+                      }),
                 ),
                 Positioned(
                   top: 0,
@@ -61,11 +96,10 @@ class ImageList extends StatelessWidget {
                   width: 40,
                   height: 40,
                   child: IconButton(
-                    padding: EdgeInsets.all(4),
-                    onPressed: () {},
-                    icon: Icon(Icons.remove_circle),
-                    color: Colors.black38
-                  ),
+                      padding: EdgeInsets.all(4),
+                      onPressed: () {},
+                      icon: Icon(Icons.remove_circle),
+                      color: Colors.black38),
                 ),
               ],
             ),
