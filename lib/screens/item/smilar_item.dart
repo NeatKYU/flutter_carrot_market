ac868841931302a8f26e4a07116a5e3b3bc7c255
kyu <2066apple@naver.com>
2022-09-23 23:26:10 +0900

    ✨ smilar_item 클릭시 detail페이지로 이동

--- lib/screens/item/smilar_item.dart
+++ lib/screens/item/smilar_item.dart
@@ -1,6 +1,7 @@
 import 'package:carrot_market_by_flutter/model/item_model/item_model.dart';
 import 'package:extended_image/extended_image.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class SmilarItem extends StatelessWidget {
   final ItemModel _itemModel;
@@ -8,38 +9,43 @@ class SmilarItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: [
-        AspectRatio(
-          aspectRatio: 4 / 3,
-          child: ExtendedImage.network(
-            _itemModel.imageDownloadUrls[0],
-            fit: BoxFit.cover,
-            borderRadius: BorderRadius.circular(8),
-            shape: BoxShape.rectangle,
+    return GestureDetector(
+      onTap: () {
+        GoRouter.of(context).go('/item/${_itemModel.itemKey}');
+      },
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          AspectRatio(
+            aspectRatio: 4 / 3,
+            child: ExtendedImage.network(
+              _itemModel.imageDownloadUrls[0],
+              fit: BoxFit.cover,
+              borderRadius: BorderRadius.circular(8),
+              shape: BoxShape.rectangle,
+            ),
           ),
-        ),
-        SizedBox(
-          height: 10,
-        ),
-        Text(
-          _itemModel.title,
-          overflow: TextOverflow.ellipsis,
-          maxLines: 1,
-          style: TextStyle(
-            fontSize: 16,
+          SizedBox(
+            height: 10,
           ),
-        ),
-        SizedBox(
-          height: 10,
-        ),
-        Text(
-          _itemModel.price.toString() + '원',
-          style: TextStyle(color: Colors.grey),
-        ),
-      ],
+          Text(
+            _itemModel.title,
+            overflow: TextOverflow.ellipsis,
+            maxLines: 1,
+            style: TextStyle(
+              fontSize: 16,
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Text(
+            _itemModel.price.toString() + '원',
+            style: TextStyle(color: Colors.grey),
+          ),
+        ],
+      ),
     );
   }
 }
