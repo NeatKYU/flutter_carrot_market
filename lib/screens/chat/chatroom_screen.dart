0dcd4f814399c4714c76a800a64558df84003b93
kyu <2066apple@naver.com>
2022-10-02 16:57:42 +0900

    ✨ chat layout step1

--- lib/screens/chat/chatroom_screen.dart
+++ lib/screens/chat/chatroom_screen.dart
@@ -1,3 +1,5 @@
+import 'package:carrot_market_by_flutter/constants/common_size.dart';
+import 'package:extended_image/extended_image.dart';
 import 'package:flutter/material.dart';
 
 class ChatroomScreen extends StatefulWidget {
@@ -15,8 +17,88 @@ class _ChatroomScreenState extends State<ChatroomScreen> {
       appBar: AppBar(
         title: Text('chat room'),
       ),
-      body: Center(
-        child: Text(widget.chatroomKey),
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ListTile(
+              leading: ExtendedImage.network(
+                'https://picsum.photos/50',
+                fit: BoxFit.cover,
+                shape: BoxShape.rectangle,
+              ),
+              title: RichText(
+                text: TextSpan(
+                  style: TextStyle(color: Colors.black),
+                  children: [
+                    TextSpan(text: '거래완료'),
+                    TextSpan(text: ' 거래 제목~'),
+                  ],
+                ),
+              ),
+              subtitle: RichText(
+                text: TextSpan(
+                  style: TextStyle(color: Colors.black),
+                  children: [
+                    TextSpan(text: '30000원'),
+                    TextSpan(text: ' (가격제안불가)'),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: common_padding),
+              child: TextButton.icon(
+                icon: Icon(Icons.edit),
+                label: Text('후기 남기기'),
+                onPressed: () {
+                  
+                },
+              ),
+            ),
+            Expanded(
+              child: Center(
+                child: Text(widget.chatroomKey),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: common_padding_sm),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.add),
+                    color: Colors.grey,
+                  ),
+                  Expanded(
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                          hintText: '메시지를 입력하세요.',
+                          isDense: true,
+                          contentPadding: EdgeInsets.only(
+                            left: common_padding,
+                            right: common_padding_sm,
+                            top: common_padding_sm,
+                            bottom: common_padding_sm,
+                          ),
+                          suffixIcon: Icon(Icons.emoji_emotions_outlined),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20),
+                            borderSide: BorderSide(color: Colors.grey),
+                          )),
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.send),
+                    color: Colors.grey,
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
